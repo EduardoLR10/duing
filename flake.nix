af6df993c7b867e806068112ea54a88e651fb283
EduardoLR10 <dudulr10@gmail.com>
2023-01-03 12:56:39 -0300

    Removed images from repo

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "~glorifiedgluer's website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          inherit (pkgs)
+            emacs-nox
+            emacsPackagesFor
+            hugo
+            hut
+
+            lib
+            mkShell
+            stdenv
+            ;
+        in
+        {
+          packages.default =
+            let
+              customEmacs = (emacsPackagesFor emacs-nox).emacsWithPackages
+                (epkgs: with epkgs.melpaPackages; [
+                  ox-hugo
+                ]
+                ++ (with epkgs.elpaPackages; [
+                  org
+                ]));
+            in
+            stdenv.mkDerivation {
+              name = "glorifiedgluercom";
+              src = lib.cleanSource ./.;
+
+              buildInputs = [
+                customEmacs
+                hugo
+              ];
+
+              configurePhase = ''
+                cd content
+                emacs $(pwd) --batch -load export.el
+                cd ..
+              '';
+
+              buildPhase = ''
+                hugo
+              '';
+
+              installPhase = ''
+                mkdir -p $out
+                cp -r public/* $out
+              '';
+            };
+
+          devShells.default = mkShell {
+            buildInputs = [
+              hut
+              hugo
+            ];
+          };
+        });
+}
